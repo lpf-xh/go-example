85151d919eeb80f7cb6bc6aa6e8a46f4e0c297fd
deepfoc <deepfoc@outlook.com>
2020-07-30 09:51:23 +0800

    add reflect example

---
+++ reflect/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type point struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+func (p *point) Print() {
+	fmt.Println("point is",p)
+}
+
+func main() {
+	a := point{
+		X: 1,
+		Y: 1,
+	}
+
+	fmt.Println("original point:",a)
+
+	// 获取 reflect.Value
+	av := reflect.ValueOf(&a)
+	// 获取 reflect.Type
+	at := av.Type()
+
+	// 修改字段值
+	// 使用Elem获取指针对应的值，才能修改
+	av.Elem().FieldByName("Y").SetInt(2)
+	fmt.Println("modified point:",a)
+
+	fmt.Println()
+	
+	// 打印类型
+	fmt.Println("av kind:", av.Kind())
+	fmt.Println("at kind:", at.Kind())
+	fmt.Println("av.Elem kind:", av.Elem().Kind())
+	fmt.Println("at.Elem kind:", at.Elem().Kind())
+	
+
+	fmt.Println()
+
+	// 打印字段名称和tag
+	for i := 0; i < at.Elem().NumField(); i++ {
+		f := at.Elem().Field(i)
+		fmt.Printf("field %d name: %s\n", i, f.Name)
+		fmt.Printf("field %d tag:  %s\n", i, f.Tag.Get("json"))
+	}
+
+	fmt.Println()
+
+	// 调用方法
+	av.Method(0).Call(nil)
+}
+
+
